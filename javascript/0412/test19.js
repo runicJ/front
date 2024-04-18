073d42b4adeea9eb60f1aeaff1a17c751d6a3da9
Dodo <jhs2468@naver.com>
2024-04-18 17:50:47 +0900

    20240418_1750

--- javascript/0412/test19.js
+++ javascript/0412/test19.js
@@ -20,7 +20,7 @@ function regexCheck() {
   const regex22 = /\d/g;  // 숫자를 포함하고 있느냐?
   const regex23 = /\D/g;  // 숫자를 포함하고 있지 않느냐?(^와 같은 의미)
   const regex24 = /\w/g;  // 영문자/숫자/_을 포함하고 있느냐?
-  const regex25 = /\W/g;  // 영문자/숫자/_만 포함하고 있느냐?
+  const regex25 = /\W/g;  // 영문자/숫자/_를 제외한 것을 포함하고 있느냐?
   const regex26 = /\s/g;  // 문장 안에 공백(탭문자 포함)을 포함하고 있느냐?
   
   let content = document.getElementById("content").value.trim();
